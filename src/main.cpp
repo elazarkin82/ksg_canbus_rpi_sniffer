4c1f51de1d57495e4cb270c73bb76c96af93102f
elazarkin <elazarkin@gmail.com>
2026-03-05 00:14:32 +0200

    implemented sniffer runner

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
+#include "core/MainService.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
 
-int main() {
-    std::cout << "TODO create raspberry pi canbus sniffer project" << std::endl;
+core::MainService* g_service = nullptr;
+
+void signal_handler(int sig)
+{
+    printf("\nReceived signal %d, stopping...\n", sig);
+    if (g_service)
+    {
+        g_service->stop();
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    const char* configPath = "sniffer.prop";
+    if (argc > 1)
+    {
+        configPath = argv[1];
+    }
+
+    printf("Starting RpiCanbusSniffer with config: %s\n", configPath);
+
+    signal(SIGINT, signal_handler);
+    signal(SIGTERM, signal_handler);
+
+    g_service = new core::MainService(configPath);
+
+    // Run the service (blocking)
+    g_service->run();
+
+    delete g_service;
+    g_service = nullptr;
+
+    printf("Exiting...\n");
     return 0;
 }
