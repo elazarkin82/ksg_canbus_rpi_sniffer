df1fa46ab516f1e78e4d12926f68f82a0dc78076
elazarkin <elazarkin@gmail.com>
2026-05-07 10:04:50 +0300

    add status manager for implement time in messages and status bar in python

---
+++ include/utils/StatusManager.hpp
@@ -0,0 +1,112 @@
+#ifndef STATUS_MANAGER_HPP
+#define STATUS_MANAGER_HPP
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <chrono>
+#include <mutex>
+#include <unordered_map>
+#include <string>
+
+namespace utils
+{
+
+class StatusManager
+{
+public:
+    static StatusManager& getInstance()
+    {
+        static StatusManager instance;
+        return instance;
+    }
+
+    // Prevent copying and assignment
+    StatusManager(const StatusManager&) = delete;
+    StatusManager& operator=(const StatusManager&) = delete;
+
+    double get_time_ms_from_start()
+    {
+        uint64_t nowUsec;
+        uint64_t diffUsec;
+        
+        nowUsec = (uint64_t)std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::steady_clock::now().time_since_epoch()
+        ).count();
+        
+        diffUsec = nowUsec - m_startTimeUsec;
+        return (double)diffUsec / 1000.0;
+    }
+
+    void update_status(const char* key, const char* message_status)
+    {
+        std::string sKey;
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        if (!key || !message_status)
+        {
+            return;
+        }
+
+        sKey = key;
+        // If the key is new, we ensure the string has the requested capacity
+        if (m_statusMessages.find(sKey) == m_statusMessages.end())
+        {
+            m_statusMessages[sKey].reserve(DEFAULT_MSG_CAPACITY);
+        }
+        
+        m_statusMessages[sKey] = message_status;
+    }
+
+    void get_status(char* out_status, uint32_t max_size)
+    {
+        size_t currentLen;
+        int written;
+        std::unordered_map<std::string, std::string>::iterator it;
+
+        std::lock_guard<std::mutex> lock(m_mutex);
+
+        if (!out_status || max_size == 0)
+        {
+            return;
+        }
+
+        out_status[0] = '\0';
+        currentLen = 0;
+
+        for (it = m_statusMessages.begin(); it != m_statusMessages.end(); ++it)
+        {
+            written = snprintf(out_status + currentLen, (size_t)max_size - currentLen, "%s: %s\n", it->first.c_str(), it->second.c_str());
+            
+            if (written > 0)
+            {
+                currentLen += (size_t)written;
+            }
+
+            if (currentLen >= (size_t)max_size)
+            {
+                break;
+            }
+        }
+    }
+
+private:
+    static const uint32_t DEFAULT_MSG_CAPACITY = 256;
+
+    StatusManager()
+    {
+        m_startTimeUsec = (uint64_t)std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::steady_clock::now().time_since_epoch()
+        ).count();
+    }
+    
+    ~StatusManager() {}
+
+    uint64_t m_startTimeUsec;
+    std::unordered_map<std::string, std::string> m_statusMessages;
+    std::mutex m_mutex;
+};
+
+} // namespace utils
+
+#endif // STATUS_MANAGER_HPP
